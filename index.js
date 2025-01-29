71b1a1152c037ea0b2d20705eb2132c7670da00f
yhanaalvarez <portorhickz@gmail.com>
2025-01-30 00:35:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facebook Tools BotScope</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>📣 FACEBOOK TOOLS BOTSCOPES</h1>
+    </header>
+    
+    <main>
+        <section>
+            <h2>Available Tools</h2>
+            <ul>
+                <li><a href="https://cookies298.onrender.com">cookie getter Tool</a></li>
+                <li><a href="https://facebook-access-token-f91q.onrender.com">Facebook Access Token Tool</a></li>
+                <li><a href="https://rpliker-bfj6.onrender.com">autoreact Tool</a></li>
+             </ul>
+        </section>
+    </main>
+    
+    <footer>
+        <p>Today is Tuesday, January 1, 2025</p>
+    </footer>
+</body>
+</html>
